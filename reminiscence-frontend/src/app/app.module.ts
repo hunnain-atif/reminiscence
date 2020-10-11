b27b321745c33346615e6778a6ea2330dce4f215
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-10 22:37:36 -0400

    creating journal card component

--- reminiscence-frontend/src/app/app.module.ts
+++ reminiscence-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JournalListComponent } from './pages/journal-list/journal-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { JournalCardComponent } from './journal-card/journal-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JournalListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    JournalCardComponent
   ],
   imports: [
     BrowserModule,
