3cd01ddffc9d4029699c77c555a862a1d148f3f1
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-10 23:28:59 -0400

    adding dynamically displayed overflow gradient

--- reminiscence-frontend/src/app/journal-card/journal-card.component.ts
+++ reminiscence-frontend/src/app/journal-card/journal-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-journal-card',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JournalCardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
   }
 
 }
