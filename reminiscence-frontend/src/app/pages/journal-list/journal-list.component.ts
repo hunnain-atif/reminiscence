1e7ece164872e6a132d3609a5a1381e5284a5c6f
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-13 14:13:07 -0400

    add animation for onCreate

--- reminiscence-frontend/src/app/pages/journal-list/journal-list.component.ts
+++ reminiscence-frontend/src/app/pages/journal-list/journal-list.component.ts
@@ -1,11 +1,38 @@
+import { animate, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Journal } from 'src/app/shared/journal.model';
 import { JournalsService } from 'src/app/shared/journals.service';
+import { trigger, transition, style, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-journal-list',
   templateUrl: './journal-list.component.html',
-  styleUrls: ['./journal-list.component.scss']
+  styleUrls: ['./journal-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*',
+        })),
+        animate(68)
+      ])
+    ])
+  ]
 })
 export class JournalListComponent implements OnInit {
 
