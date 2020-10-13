5aacec15dd0be1da6304d206120a3132b4e82115
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-13 12:59:30 -0400

    create journals service

---
+++ reminiscence-frontend/src/app/shared/journals.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { notEqual } from 'assert';
+import { Journal } from './journal.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JournalsService {
+
+  journals: Journal[] = new Array<Journal>();
+
+
+  constructor() { }
+
+  get(id: number) {
+    return this.journals[id];
+  }
+
+  getId(journal: Journal) {
+    return this.journals.indexOf(journal);
+  }
+
+  add(journal: Journal) {
+    let length = this.journals.push(journal);
+    //returning index
+    return length - 1;
+  }
+
+  update(id: number, title:string, body: string) {
+    let journal = this.journals[id];
+    journal.date = title;
+    journal.body = body;
+  }
+
+  delete(id: number) {
+    this.journals.splice(id, 1);
+  }
+}
